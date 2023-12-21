7ca4f921d7bea642053e1b466cac76cf1426d7f8
Paul-Lac <plnlacroix@gmail.com>
2023-12-21 17:30:31 +0100

    Quete CSS dans React

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.scss'
+
+function App() {
+  return (
+    <>
+    <card>
+      <div className="top-container">
+          <img src="src\assets\birds.png" alt="birds" />
+          <div className="desc-container">
+            <h1>Cute little birds</h1>
+            <p>Hello cutties, what are you looking at ?</p>
+          </div>
+      </div>
+      <div className="bottom-container">
+        <img src="src\assets\heart.png" alt="birds" />
+        <p>What a really nice card</p>
+        <img src="src\assets\heart.png" alt="birds" />
+      </div>
+    </card>
+    </>
+  )
+}
+
+export default App;
